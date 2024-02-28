b2fd635db0f17e94310c01b7b0e31e07f0cc1492
adeFuLoDgu <64475795+adeFuLoDgu@users.noreply.github.com>
2024-02-28 14:25:17 +0800

    Update TwitchDefaultSortbyViewerCount.user.js

--- TwitchDefaultSortbyViewerCount.user.js
+++ TwitchDefaultSortbyViewerCount.user.js
@@ -1,21 +1,23 @@
 // ==UserScript==
 // @name        Twitch Default Sort by Viewer Count
 // @namespace   https://github.com/adeFuLoDgu/Twitch-Default-Sort-by-Viewer-Count
-// @version     0.1
+// @version     0.2
 // @description Set default sort to viewer count on Twitch directory pages.
 // @author      adeFuLoDgu
-// @match     	*://*.twitch.tv/directory/category/*
+// @match     	*://*.twitch.tv/*
 // @updateURL   https://github.com/adeFuLoDgu/Twitch-Default-Sort-by-Viewer-Count/raw/main/TwitchDefaultSortbyViewerCount.user.js
 // @downloadURL https://github.com/adeFuLoDgu/Twitch-Default-Sort-by-Viewer-Count/raw/main/TwitchDefaultSortbyViewerCount.user.js
-// @run-at      document-end
+// @run-at      document-start
 // @grant       none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    let url = window.location.href;
-    if(url.indexOf("?sort=VIEWER_COUNT") == -1) {
-        url += "?sort=VIEWER_COUNT";
-        window.location.href = url;
-    }
+    setInterval(() => {
+        let url=window.location.href;
+        if(url.indexOf("directory/category")!=-1&&url.indexOf("?sort=")==-1&&url.indexOf("/videos")==-1&&url.indexOf("/clips")==-1) {
+            url += "?sort=VIEWER_COUNT";
+            window.location.href = url;
+        }
+    }, 1000);
 })();
