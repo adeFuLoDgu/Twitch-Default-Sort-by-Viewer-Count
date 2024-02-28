7a153dc9cc328b136ee13c9203a0a966d5091912
adeFuLoDgu <64475795+adeFuLoDgu@users.noreply.github.com>
2024-02-28 12:35:58 +0800

    Update TwitchDefaultSortbyViewerCount.user.js

--- TwitchDefaultSortbyViewerCount.user.js
+++ TwitchDefaultSortbyViewerCount.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name        Twitch Default Sort by Viewer Count
-// @namespace   https://github.com/adeFuLoDgu/Auto-Claim-Twitch-Channel-Point
+// @namespace   https://github.com/adeFuLoDgu/Twitch-Default-Sort-by-Viewer-Count
 // @version     0.1
 // @description Set default sort to viewer count on Twitch directory pages.
 // @author      adeFuLoDgu
 // @match     	*://*.twitch.tv/directory/category/*
-// @updateURL   https://github.com/adeFuLoDgu/Auto-Claim-Twitch-Channel-Point/raw/main/AutoTwitchPoint.user.js
-// @downloadURL https://github.com/adeFuLoDgu/Auto-Claim-Twitch-Channel-Point/raw/main/AutoTwitchPoint.user.js
+// @updateURL   https://github.com/adeFuLoDgu/Twitch-Default-Sort-by-Viewer-Count/raw/main/TwitchDefaultSortbyViewerCount.user.js
+// @downloadURL https://github.com/adeFuLoDgu/Twitch-Default-Sort-by-Viewer-Count/raw/main/TwitchDefaultSortbyViewerCount.user.js
 // @run-at      document-end
 // @grant       none
 // ==/UserScript==
